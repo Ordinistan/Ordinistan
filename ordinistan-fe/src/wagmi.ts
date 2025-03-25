110c9b4775acd2609124712d5db8f6176dd56b5c
Muhammad Shahrukh Azeem <89108936+Muhammad-Shahrukh-Azeem@users.noreply.github.com>
2025-03-25 18:05:35 +0500

    sepolia added to config

--- ordinistan-fe/src/wagmi.ts
+++ ordinistan-fe/src/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { coreDao } from "wagmi/chains";
+import { coreDao, sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "RainbowKit App",
   projectId: "YOUR_PROJECT_ID",
-  chains: [coreDao],
+  chains: [coreDao, sepolia],
   ssr: true,
 });
